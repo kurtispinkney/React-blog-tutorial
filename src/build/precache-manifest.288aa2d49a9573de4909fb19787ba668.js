c7c8a9d852aadc3ac64a64b52568f34b5d8e1b1a
Kurtis Pinkney <kurtis.pinkney@gmail.com>
2020-06-15 21:24:29 -0400

    Completed app

---
+++ src/build/precache-manifest.288aa2d49a9573de4909fb19787ba668.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cfe56e6feafb24d2d33ca6c0a8dbca3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "199650b573d8929b04c1",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "239c186af79c23f6a9be",
+    "url": "/static/js/2.9961cdb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9961cdb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199650b573d8929b04c1",
+    "url": "/static/js/main.c615a00b.chunk.js"
+  },
+  {
+    "revision": "c599d4e707eafd705e3f",
+    "url": "/static/js/runtime-main.bc9f3333.js"
+  }
+]);
